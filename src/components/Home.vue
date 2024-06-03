b2130cab8a0ca95a5926d21bba86a09a9702a674
Constantinos Paschali <constantinos_paschali@hotmail.com>
2024-06-03 07:04:01 +0300

    switch to table

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -29,7 +29,31 @@
     </div>
     <v-tabs-window v-model="tab">
       <v-tabs-window-item value="1" class="list">
-        <PokemonList :filter="filter?.toLowerCase()" :types="selectedTypes"/>
+        <v-data-table-virtual
+          :headers="headers"
+          :items="pokemons"
+          height="100%"
+        >
+          <template #[`item.image`]="{item}">
+            <v-avatar :image="item.image"/>
+          </template>
+          <template #[`item.name`]="{item}">
+            <p class="text-capitalize">{{ item.name }}</p>
+          </template>
+          <template #[`item.types`]="{item}">
+            <p class="text-capitalize">{{ item.types.join(', ') }}</p>
+          </template>
+          <template #[`item.actions`]="{item}">
+            <v-btn-group variant="plain" class="d-flex w-100 justify-center">
+              <v-btn variant="plain" @click="view(item.id)">View</v-btn>
+              <v-btn variant="plain" symbol="star" @click="handleFavourite(item)">
+                Favourite
+                <v-icon color="yellow" :icon="store.isFavourite(item.id) ? 'mdi-star' :'mdi-star-outline'" end/>
+              </v-btn>
+            </v-btn-group>
+          </template>
+        </v-data-table-virtual>
+        <!--        <PokemonList :filter="filter?.toLowerCase()" :types="selectedTypes"/>-->
       </v-tabs-window-item>
       <v-tabs-window-item value="2" class="list">
         <div class="mt-2 mx-2 d-flex flex-wrap justify-center flex-1-1-100" v-if="favs.length">
@@ -50,7 +74,10 @@
 import { useFavouriteStore } from '../stores/favourites'
 import PokemonCard from './PokemonCard.vue'
 import { types } from '../variables/types'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import gql from 'graphql-tag'
+import { useQuery } from '@vue/apollo-composable'
+import { useRouter } from 'vue-router/auto';
 
 export default {
   components: { PokemonCard },
@@ -58,6 +85,55 @@ export default {
     const store = useFavouriteStore()
     const typeNames = Object.keys(types)
     const typeColors = Object.values(types)
+    const headers = [
+      { title: 'ID', align: 'start', key: 'id' },
+      { title: 'image', align: 'start', key: 'image' },
+      { title: 'Name', align: 'start', key: 'name' },
+      { title: 'Types', align: 'start', key: 'types' },
+      { title: 'Generation', align: 'start', key: 'generation' },
+      { title: 'Actions', align: 'center', key: 'actions' },
+    ]
+    const router = useRouter();
+    const POKEMON_QUERY = gql`
+  query Pokemon {
+    pokemon_v2_pokemon  {
+    name
+    id
+    pokemon_v2_pokemonspecy {
+      generation_id
+    }
+    pokemon_v2_pokemontypes {
+      pokemon_v2_type {
+        name
+      }
+    }
+  }
+ }`
+    const { result, loading, error } = useQuery(POKEMON_QUERY)
+    const pokemons = computed(() => {
+      if (result.value && result.value.pokemon_v2_pokemon && result.value.pokemon_v2_pokemon.length) {
+        let simplified = result.value.pokemon_v2_pokemon
+        if (filter.value && filter.value.length) {
+          simplified = result.value.pokemon_v2_pokemon.filter(pokemon => pokemon.name.includes(filter.value))
+        }
+        if (selectedTypes.value && selectedTypes.value.length) {
+          simplified = result.value.pokemon_v2_pokemon.filter(pokemon => {
+            const pokemonTypes = pokemon.pokemon_v2_pokemontypes.map(t => t.pokemon_v2_type.name)
+            return selectedTypes.value.every(filter => pokemonTypes.includes(filter))
+          })
+        }
+        return simplified.map(pokemon => {
+          return {
+            'id': pokemon.id,
+            'name': pokemon.name,
+            'generation': pokemon.pokemon_v2_pokemonspecy.generation_id,
+            'types': pokemon.pokemon_v2_pokemontypes.map(type => type.pokemon_v2_type.name),
+            'image': `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`,
+          }
+        })
+      }
+    })
+
     const favs = computed(() => {
       const filtered = store.sortedFavourites.filter(sortedPoke => sortedPoke.name.includes(filter.value.toLowerCase()))
       console.log(filtered)
@@ -81,6 +157,18 @@ export default {
       }
     }
 
+    function handleFavourite(pokemon) {
+      if (store.isFavourite(pokemon.id)) {
+        store.removePokemon(pokemon.id)
+      } else {
+        store.addPokemon(pokemon)
+      }
+    }
+
+    function view(id) {
+      router.push(`/pokemon/${id}`)
+    }
+
     return {
       store,
       filter,
@@ -88,7 +176,11 @@ export default {
       typeNames,
       typeColors,
       selectedTypes,
+      headers,
+      pokemons,
       filterByType,
+      handleFavourite,
+      view,
     }
   },
   data: () => ({
